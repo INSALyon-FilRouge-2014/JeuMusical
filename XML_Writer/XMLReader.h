e1c91e15e612af8947f517c533316ad3e8074f31
estlx <elepeigneux@gmail.com>
2015-04-22 12:38:49 +0200

    Ajout XML Reader

---
+++ XML_Writer/XMLReader.h
@@ -0,0 +1,23 @@
+#ifndef XMLREADER_H_
+#define XMLREADER_H_
+
+#include <fstream>
+#include <string>
+#include <list>
+#include <vector>
+#include "Obstacle.h"
+#include "tinyxml.h"
+
+class XMLReader
+{
+public:
+	XMLReader(char* str);
+	~XMLReader();
+	//Lis toutes les balises de type "bloc" et place les objets ainsi créés dans un tableau
+	std::vector<Obstacle>* read();
+protected:
+	TiXmlDocument doc;
+};
+
+#endif
+
