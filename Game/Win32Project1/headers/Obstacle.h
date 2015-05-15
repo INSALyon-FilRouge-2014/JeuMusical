9d9a6633884be8ba5468019b281935a1d63e0ed7
Loïc Touzard <loic.touzard@gmail.com>
2015-05-15 13:01:30 +0200

    Modification du obstacle.h chez alexis

--- Game/Win32Project1/headers/Obstacle.h
+++ Game/Win32Project1/headers/Obstacle.h
@@ -1,8 +1,5 @@
-#ifndef OBSTACLE_H_
-#define OBSTACLE_H_
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef OBSTACLE_H
+#define OBSTACLE_H
 
 class Obstacle
 {
@@ -10,20 +7,29 @@ class Obstacle
 public:
 	enum TypeObstacle { CAISSE, NUAGE, PIC };
 
-	int getPosition(){return position;}
+	unsigned int getPosition(){ return position; }
 
-	int getHauteur(){return hauteur;}
+	unsigned int getPCMPosition(){ return PCMPosition; }
 
-	int getType(){return type;}
+	unsigned int getPCM1024Position(){ return unsigned int(PCMPosition / 1024.f); }
 
-	Obstacle(int pos, int h, TypeObstacle typ)
-		:position(pos),hauteur(h),type(typ){}
+	int getHauteur(){ return hauteur; }
+
+	TypeObstacle getType(){ return type; }
+
+	Obstacle(unsigned int pos, unsigned int pcmpos, int h, TypeObstacle typ)
+		:position(pos), PCMPosition(pcmpos), hauteur(h), type(typ){}
+
+	virtual ~Obstacle(){}
 
 private:
-	int position;//position en X
+	unsigned int position;//position en X
+
+	unsigned int PCMPosition;//position de l'obstacle en PCM de la musique
+	//NB : PCM / 1024 regrouppement des echantillons par 1024
 
 	int hauteur;//position en Y
-	
+
 	TypeObstacle type;
 
 };
